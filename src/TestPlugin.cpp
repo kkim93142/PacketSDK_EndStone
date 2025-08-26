8b47ba414e4beef5abf0edbd9d43b5fea8bbb7e6
kkim93142 <kkim93142@gmail.com>
2025-08-26 18:04:14 +0900

    wtf?

--- src/TestPlugin.cpp
+++ src/TestPlugin.cpp
@@ -5,4 +5,5 @@ ENDSTONE_PLUGIN("test_plugin", "0.1.0", TestPlugin)
     description = "Test Plugin";
 
     command("test");
+    command("pcheck2");
 }
--- src/TestPlugin.h
+++ src/TestPlugin.h
@@ -6,6 +6,7 @@
 
 #include <endstone/endstone.hpp>
 #include <endstone/event/server/packet_receive_event.h>
+#include <chrono>
 
 class TestPlugin : public endstone::Plugin {
     void onEnable() override
@@ -17,32 +18,32 @@ class TestPlugin : public endstone::Plugin {
 
     bool onCommand(endstone::CommandSender &sender, const endstone::Command &command, const std::vector<std::string> &args) override
     {
-        if (command.getName() == "test") {
-            getLogger().info("command test called");
+        // if (command.getName() == "test") {
+        //     getLogger().info("command test called");
+        //
+        // }
 
-        }
-        // sender.asPlayer();
         return true;
     }
 
     void onPacket(endstone::PacketReceiveEvent& event)
     {
-        if (event.getPacketId() == static_cast<int>(MinecraftPacketIds::Text)) {
-            auto textPacket = TextPacket(event.getPayload());
-            getLogger().info("TextPacket received");
-            getLogger().info("Source Name: {}", textPacket.sourceName.value());
-            getLogger().info("message: {}", textPacket.message);
-            getLogger().info("xuid: {}", textPacket.xuid);
-        }
-
-        if (event.getPacketId() == BookEditPacket::packetIdAsInt) {
-            auto bookEditPacket = BookEditPacket(event.getPayload());
-            getLogger().info("BookEditPacket received");
-            getLogger().info("Action: {}", static_cast<int>(bookEditPacket.type));
-            getLogger().info("xuid: {}", bookEditPacket.xuid.value_or("nothing"));
-            getLogger().info("text: {}", bookEditPacket.text.value_or("nothing"));
-            getLogger().info("slot: {}", bookEditPacket.slot);
-        }
+        // if (event.getPacketId() == static_cast<int>(MinecraftPacketIds::Text)) {
+        //     auto textPacket = TextPacket(event.getPayload());
+        //     getLogger().info("TextPacket received");
+        //     getLogger().info("Source Name: {}", textPacket.sourceName.value());
+        //     getLogger().info("message: {}", textPacket.message);
+        //     getLogger().info("xuid: {}", textPacket.xuid);
+        // }
+        //
+        // if (event.getPacketId() == BookEditPacket::packetIdAsInt) {
+        //     auto bookEditPacket = BookEditPacket(event.getPayload());
+        //     getLogger().info("BookEditPacket received");
+        //     getLogger().info("Action: {}", static_cast<int>(bookEditPacket.type));
+        //     getLogger().info("xuid: {}", bookEditPacket.xuid.value_or("nothing"));
+        //     getLogger().info("text: {}", bookEditPacket.text.value_or("nothing"));
+        //     getLogger().info("slot: {}", bookEditPacket.slot);
+        // }
     }
 
     void onPacketSend(endstone::PacketSendEvent& event) {
